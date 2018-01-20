efbcaa5bc95bf6396b3133de79bab537215f6707
Gary Lee <leechisan2007@hotmail.com>
2018-01-20 20:17:44 +0800

    3. Longest Substring Without Repeating Characters

---
+++ LeetCode/Problem3/Solution.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Problem3
+{
+    public class Solution
+    {
+        public int LengthOfLongestSubstring(string s)
+        {
+            if(s == string.Empty)
+                return 0;
+
+            int low = 0, high = 0, answer = 0;
+            var dic = new Dictionary<char, int>();
+
+            while(high < s.Length)
+            {
+                char c = s[high];
+                dic.TryAdd(c, 0);
+
+                dic[c]++;
+                if(dic[c] == 2)
+                {
+                    while(true)
+                    {
+                        low++;
+                        dic[s[low - 1]]--;
+                        if(s[low - 1] == c)
+                            break;
+                    }
+                }
+
+                answer = Math.Max(answer, high - low + 1);
+                high++;
+            }
+
+            return answer;
+        }
+    }
+}
---
+++ LeetCode/Problem3/Test.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using FluentAssertions;
+
+namespace LeetCode.Problem3
+{
+    public class SolutionTest
+    {
+        [Theory]
+        [InlineData("", 0, "")]
+        [InlineData("abcabcbb", 3, "abc")]
+        [InlineData("bbbbb", 1, "b")]
+        [InlineData("pwwkew", 3, "wke")]
+        [InlineData("tmmzuxt", 5, "mzuxt")]
+        public void Test(string input, int expected, string possibleSubstring)
+        {
+            var actual = new Solution().LengthOfLongestSubstring(input);
+
+            actual.Should().Be(expected, $"'{input}' --> '{possibleSubstring}'");
+        }
+    }
+}
