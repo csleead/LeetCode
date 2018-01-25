b1987bc2b6a27c6d32f4a0a1d07bc27cc9126fdc
Gary Lee <leechisan2007@hotmail.com>
2018-01-25 21:05:17 +0800

    605. Can Place Flowers

---
+++ LeetCode/Problem601/Solution.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace LeetCode.Problem601
+{
+    public class Solution
+    {
+        public bool CanPlaceFlowers(int[] flowerbed, int n)
+        {
+            return n <= CalcMaxNumOfFlower(flowerbed);
+        }
+
+        private int CalcMaxNumOfFlower(int[] flowerbed)
+        {
+            int i = 0, sum = 0;
+            while(i < flowerbed.Length)
+            {
+                if(flowerbed[i] == 0 &&
+                    flowerbed.ElementAtOrDefault(i - 1) == 0 &&
+                    flowerbed.ElementAtOrDefault(i + 1) == 0)
+                {
+                    sum++;
+                    i += 2;
+                }
+                else
+                {
+                    i++;
+                }
+            }
+            return sum;
+        }
+    }
+}
---
+++ LeetCode/Problem601/SolutionTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentAssertions;
+using Xunit;
+
+namespace LeetCode.Problem601
+{
+    public class SolutionTest
+    {
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public void Test(int[] flowerbed, int n, bool expected)
+        {
+            var actual = new Solution().CanPlaceFlowers(flowerbed, n);
+
+            actual.Should().Be(expected);
+        }
+
+        public static IEnumerable<object[]> TestData()
+        {
+            yield return new object[] { new[] { 0 }, 1, true };
+            yield return new object[] { new[] { 1 }, 1, false };
+            yield return new object[] { new[] { 0 }, 2, false };
+            yield return new object[] { new[] { 1, 0, 1 }, 1, false };
+            yield return new object[] { new[] { 1, 0, 0, 1 }, 1, false };
+            yield return new object[] { new[] { 1, 1, 0, 0 }, 1, true };
+            yield return new object[] { new[] { 1, 0, 0, 0, 1 }, 1, true };
+            yield return new object[] { new[] { 1, 0, 0, 0, 1 }, 2, false };
+            yield return new object[] { new[] { 1, 0, 1, 0, 1 }, 1, false };
+            yield return new object[] { new[] { 1, 0, 0, 0, 0, 0, 1 }, 2, true };
+            yield return new object[] { new[] { 0, 0, 0, 0 }, 2, true };
+        }
+    }
+}
