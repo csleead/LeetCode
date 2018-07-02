06c665eb0aaa0135b8f31696f22a3ffa62a02226
Gary Lee <leechisan2007@hotmail.com>
2018-07-02 21:14:52 +0800

    789

---
+++ LeetCode/Problem789/Solution.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Problem789
+{
+    public class Solution
+    {
+        public bool EscapeGhosts(int[][] ghosts, int[] target)
+        {
+            var timeToEscape = Distance(new[] { 0, 0 }, target);
+            foreach(var ghost in ghosts)
+            {
+                if(Distance(ghost, target) < timeToEscape)
+                    return false;
+            }
+
+            return true;
+        }
+
+        private int Distance(int[] a, int[] b)
+        {
+            return Math.Abs(a[0] - b[0]) + Math.Abs(a[1] - b[1]);
+        }
+    }
+}
