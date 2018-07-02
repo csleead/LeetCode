68484f1a133b2b4822451b75a1388a81206b9bcb
Gary Lee <leechisan2007@hotmail.com>
2018-07-02 21:01:10 +0800

    540

---
+++ LeetCode/Problem540/Solution.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Problem540
+{
+    public class Solution
+    {
+        public int SingleNonDuplicate(int[] nums)
+        {
+            int low = 0, up = nums.Length - 1;
+
+            while(true)
+            {
+                if(low == up)
+                    return nums[low];
+
+                int mid = (low + up) / 2;
+                if(nums[mid] != nums[mid + 1] && nums[mid] != nums[mid - 1])
+                    return nums[mid];
+
+                if(mid % 2 == 0)
+                {
+                    if(nums[mid] == nums[mid + 1])
+                        low = mid + 2;
+                    else
+                        up = mid;
+                }
+                else
+                {
+                    if(nums[mid] == nums[mid - 1])
+                        low = mid + 1;
+                    else
+                        up = mid - 1;
+                }
+            }
+        }
+    }
+}
---
+++ LeetCode/Problem540/SolutionTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentAssertions;
+using Xunit;
+
+namespace LeetCode.Problem540
+{
+    public class SolutionTest
+    {
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public void Test(int[] input, int expected) => new Solution().SingleNonDuplicate(input).Should().Be(expected);
+
+        public static TheoryData<int[], int> TestData()
+        {
+            var data = new TheoryData<int[], int>
+            {
+                { new[] { 1 }, 1 },
+                { new[] { 1, 1, 2 }, 2 },
+                { new[] { 1, 2, 2 }, 1 },
+                { new[] { 1, 1, 2, 3, 3, 4, 4, 8, 8 }, 2 },
+                { new[] { 3, 3, 7, 7, 10, 11, 11 }, 10 }
+            };
+
+            return data;
+        }
+    }
+}
