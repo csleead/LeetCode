9053879baa418bd62e502fb8b4a91e0622c7cc55
Gary Lee <leechisan2007@hotmail.com>
2018-07-07 21:47:53 +0800

    739

---
+++ LeetCode/Problem739/Solution.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Problem739
+{
+    public class Solution
+    {
+        private static readonly int MaxTemperature = 100;
+
+        public int[] DailyTemperatures(int[] temperatures)
+        {
+            var earliestTempDic = new Dictionary<int, int>();
+            int[] result = new int[temperatures.Length];
+            for(int i = temperatures.Length - 1; i >= 0; i--)
+            {
+                var warmerIndex = FindSmallestIndexOfWarmerTemperature(temperatures[i], earliestTempDic);
+                result[i] = (warmerIndex == -1) ? 0 : warmerIndex - i;
+
+                earliestTempDic[temperatures[i]] = i;
+            }
+
+            return result;
+        }
+
+        private static int FindSmallestIndexOfWarmerTemperature(int temperature, Dictionary<int, int> earliestTempDic)
+        {
+            int result = int.MaxValue;
+            for(int t = temperature + 1; t <= MaxTemperature; t++)
+            {
+                var index = earliestTempDic.GetValueOrDefault(t);
+                if(index != 0)
+                {
+                    result = Math.Min(result, index);
+                }
+            }
+
+            return result == int.MaxValue ? -1 : result;
+        }
+    }
+}
---
+++ LeetCode/Problem739/SolutionTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace LeetCode.Problem739
+{
+    public class SolutionTest
+    {
+        [Theory]
+        [MemberData(nameof(TestCases))]
+        public void Test(int[] input, int[] expected)
+        {
+            Assert.Equal(expected, new Solution().DailyTemperatures(input));
+        }
+
+        public static TheoryData<int[], int[]> TestCases()
+        {
+            var testCases = new TheoryData<int[], int[]>
+            {
+                { new int[0], new int[0] },
+                { new[] { 50 }, new[] { 0 } },
+                { new[] { 50, 50 }, new[] { 0, 0 } },
+                { new[] { 50, 51 }, new[] { 1, 0 } },
+                { new[] { 50, 49, 51 }, new[] { 2, 1, 0 } },
+                { new[] { 50, 52, 51 }, new[] { 1, 0, 0 } },
+                { new[] { 73, 74, 75, 71, 69, 72, 76, 73 }, new[] { 1, 1, 4, 2, 1, 1, 0, 0 } }
+            };
+
+            return testCases;
+        }
+    }
+}
