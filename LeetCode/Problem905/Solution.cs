fd2e90203ec8eb5527b34b52c748e9aa5c8fe8e0
Gary Lee <leechisan2007@hotmail.com>
2018-10-31 22:34:06 +0800

    905

---
+++ LeetCode/Problem905/Solution.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Problem905
+{
+    public class Solution
+    {
+        public int[] SortArrayByParity(int[] A)
+        {
+            var result = new int[A.Length];
+            int odd = result.Length - 1;
+            int even = 0;
+            foreach(var i in A)
+            {
+                if(i % 2 == 0)
+                {
+                    result[even] = i;
+                    even++;
+                }
+                else
+                {
+                    result[odd] = i;
+                    odd--;
+                }
+            }
+
+            return result;
+        }
+    }
+}
