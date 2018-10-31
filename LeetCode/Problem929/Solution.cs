2c6951d98b05da0018e30142616a3f58c0840a38
Gary Lee <leechisan2007@hotmail.com>
2018-10-31 22:29:22 +0800

    929

---
+++ LeetCode/Problem929/Solution.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Problem929
+{
+    public class Solution
+    {
+        public int NumUniqueEmails(string[] emails)
+        {
+            return emails.Select(Process).ToHashSet().Count;
+        }
+
+        private static string Process(string email)
+        {
+            var split = email.Split('@');
+            split[0] = split[0].Replace(".", string.Empty);
+            if(split[0].IndexOf("+") > 0)
+            {
+                split[0] = split[0].Substring(0, split[0].IndexOf("+"));
+            }
+
+            return split[0] + split[1];
+        }
+    }
+}
---
+++ LeetCode/Problem929/SolutionTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using FluentAssertions;
+
+namespace LeetCode.Problem929
+{
+    public class SolutionTest
+    {
+        [Theory]
+        [MemberData(nameof(TestCases))]
+        public void Test(string[] emails, int expected)
+        {
+            new Solution().NumUniqueEmails(emails).Should().Be(expected);
+        }
+
+        public static TheoryData<string[], int> TestCases()
+        {
+            var data = new TheoryData<string[], int>
+            {
+                { new string[0], 0 },
+                { new [] { "abc@def.com" }, 1 },
+                { new[] { "test.email+alex@leetcode.com", "test.e.mail+bob.cathy@leetcode.com", "testemail+david@lee.tcode.com" }, 2 }
+            };
+            return data;
+        }
+    }
+}
