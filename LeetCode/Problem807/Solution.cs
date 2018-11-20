693f126e9266953f37ecac0a7fd2d96afbfb8e17
Gary Lee <leechisan2007@hotmail.com>
2018-11-20 21:43:25 +0800

    807

---
+++ LeetCode/Problem807/Solution.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Problem807
+{
+    public class Solution
+    {
+        // grid.length = grid[0].length
+        public int MaxIncreaseKeepingSkyline(int[][] grid)
+        {
+            var topViewSkyline = GetTopViewSkyline(grid);
+            var leftViewSkyline = GetLeftViewSkyline(grid);
+
+            int result = 0;
+            for(int row = 0; row < grid.Length; row++)
+            {
+                for(int col = 0; col < grid.Length; col++)
+                {
+                    int d = Math.Min(topViewSkyline[col], leftViewSkyline[row]) - grid[row][col];
+                    result += Math.Min(topViewSkyline[col], leftViewSkyline[row]) - grid[row][col];
+                }
+            }
+
+            return result;
+        }
+
+        private int[] GetTopViewSkyline(int[][] grid)
+        {
+            int n = grid.Length;
+
+            var result = new int[n];
+            for(int col = 0; col < n; col++)
+            {
+                result[col] = grid[0][col];
+                for(int row = 0; row < n; row++)
+                {
+                    result[col] = Math.Max(result[col], grid[row][col]);
+                }
+            }
+
+            return result;
+        }
+
+        private int[] GetLeftViewSkyline(int[][] grid)
+        {
+            return grid.Select(row => row.Max()).ToArray();
+        }
+    }
+}
---
+++ LeetCode/Problem807/SolutionTest.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentAssertions;
+using Xunit;
+
+namespace LeetCode.Problem807
+{
+    public class SolutionTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            var input = new int[][]
+            {
+                new[]{ 1 }
+            };
+
+            var actual = new Solution().MaxIncreaseKeepingSkyline(input);
+
+            actual.Should().Be(0);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var input = new int[][]
+            {
+                new[]{ 3, 0, 8, 4 },
+                new[]{ 2, 4, 5, 7 },
+                new[]{ 9, 2, 6, 3 },
+                new[]{ 0, 3, 1, 0 },
+            };
+
+            var actual = new Solution().MaxIncreaseKeepingSkyline(input);
+
+            actual.Should().Be(35);
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            var input = new int[][]
+            {
+                new[]{ 8, 0, 6, 4 },
+                new[]{ 2, 10, 5, 7 },
+                new[]{ 9, 2, 0, 3},
+                new[]{ 0, 7, 3, 0 },
+            };
+
+            var actual = new Solution().MaxIncreaseKeepingSkyline(input);
+
+            actual.Should().Be(53);
+        }
+    }
+}
