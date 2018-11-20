221cd5f2c0198fa7e654e4dfb96805caefa18a32
Gary Lee <leechisan2007@hotmail.com>
2018-11-20 22:06:28 +0800

    809

---
+++ LeetCode/Problem809/Solution.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Problem809
+{
+    public class Solution
+    {
+        public IList<string> FindAndReplacePattern(string[] words, string pattern)
+        {
+            return words.Where(w => IsMatch(w, pattern)).ToList();
+        }
+
+        private bool IsMatch(string word, string pattern)
+        {
+            var wordToPattern = new Dictionary<char, char>();
+            var patternToWord = new Dictionary<char, char>();
+            for(int i = 0; i < word.Length; i++)
+            {
+                if((wordToPattern.ContainsKey(word[i]) && wordToPattern[word[i]] != pattern[i]) ||
+                    (patternToWord.ContainsKey(pattern[i]) && patternToWord[pattern[i]] != word[i]))
+                {
+                    return false;
+                }
+                else
+                {
+                    wordToPattern.TryAdd(word[i], pattern[i]);
+                    patternToWord.TryAdd(pattern[i], word[i]);
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ LeetCode/Problem809/SolutionTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentAssertions;
+using Xunit;
+
+namespace LeetCode.Problem809
+{
+    public class SolutionTest
+    {
+        [Theory]
+        [MemberData(nameof(TestCases))]
+        public void Test(string[] words, string pattern, string[] expected)
+        {
+            var actual = new Solution().FindAndReplacePattern(words, pattern);
+
+            actual.Should().BeEquivalentTo(expected);
+        }
+
+        public static TheoryData<string[], string, string[]> TestCases()
+        {
+            var data = new TheoryData<string[], string, string[]>
+            {
+                { new[] { "abc", "deq", "mee", "aqq", "dkd", "ccc" }, "abb", new[] { "mee", "aqq" } },
+                { new[] { "aaa", "bbb", "ccc" }, "zzz", new[] { "aaa", "bbb", "ccc" } },
+                { new[] { "ef", "fq", "ao", "at", "lx" }, "ya", new[] { "ef","fq","ao","at","lx" } },
+            };
+
+            return data;
+        }
+    }
+}
