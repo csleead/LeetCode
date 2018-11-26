7a1860544265a124957a167a56e6523a0ff03282
Gary Lee <leechisan2007@hotmail.com>
2018-11-26 22:37:43 +0800

    303

---
+++ LeetCode/Problem303/NumArray.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Problem303
+{
+    public class NumArray
+    {
+        private int[] _sums;
+
+        public NumArray(int[] nums)
+        {
+            // For a weird test case that input an empty array
+            // and never call SumRange()
+            if(nums.Length == 0)
+                return;
+
+            _sums = new int[nums.Length];
+
+            _sums[0] = nums[0];
+            for(int i = 1; i < nums.Length; i++)
+            {
+                _sums[i] = _sums[i - 1] + nums[i];
+            }
+        }
+
+        public int SumRange(int i, int j)
+        {
+            return _sums[j] - _sums.ElementAtOrDefault(i - 1);
+        }
+    }
+}
---
+++ LeetCode/Problem303/NumArrayTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FluentAssertions;
+using Xunit;
+
+namespace LeetCode.Problem303
+{
+    public class NumArrayTest
+    {
+        [Fact]
+        public void AllowEmptyArray()
+        {
+            Action action = () => new NumArray(new int[0]);
+            action.ShouldNotThrow();
+        }
+
+        [Theory]
+        [InlineData(0, 0, 1)]
+        [InlineData(9999, 9999, 1)]
+        [InlineData(0, 9999, 10000)]
+        [InlineData(1000, 2000, 1001)]
+        [InlineData(2000, 4000, 2001)]
+        public void Test(int i, int j, int expected)
+        {
+            var arr = new NumArray(Enumerable.Repeat(1, 10000).ToArray());
+
+            arr.SumRange(i, j).Should().Be(expected);
+        }
+    }
+}
