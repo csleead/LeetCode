a546a00fb6bafc30bf516711e6dbc2ffb59c9190
Gary Lee <leechisan2007@hotmail.com>
2018-11-26 21:41:26 +0800

    746

---
+++ LeetCode/Problem746/Solution.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Problem746
+{
+    public class Solution
+    {
+        public int MinCostClimbingStairs(int[] cost)
+        {
+            var minCosts = new int[cost.Length];
+            minCosts[0] = cost[0];
+            minCosts[1] = cost[1];
+
+            for(int i = 2; i < cost.Length; i++)
+            {
+                minCosts[i] = cost[i] + Math.Min(minCosts[i - 1], minCosts[i - 2]);
+            }
+
+            return Math.Min(minCosts[cost.Length - 1], minCosts[cost.Length - 2]);
+        }
+    }
+}
---
+++ LeetCode/Problem746/SolutionTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using FluentAssertions;
+
+namespace LeetCode.Problem746
+{
+    public class SolutionTest
+    {
+        [Theory]
+        [MemberData(nameof(TestCases))]
+        public void Test(int[] cost, int expected)
+        {
+            var actual = new Solution().MinCostClimbingStairs(cost);
+            actual.Should().Be(expected);
+        }
+
+        public static TheoryData<int[], int> TestCases()
+        {
+            return new TheoryData<int[], int>
+            {
+                { new[]{ 10, 15, 20 }, 15 },
+                { new[] { 1, 100, 1, 1, 1, 100, 1, 1, 100, 1 }, 6 }
+            };
+        }
+    }
+}
