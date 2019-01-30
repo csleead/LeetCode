eec1d11d745f297a25154aed096cb73d189096bf
Gary Lee <leechisan2007@hotmail.com>
2019-01-30 21:57:31 +0800

    804

---
+++ LeetCode/Problem804/Solution.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Problem804
+{
+    public class Solution
+    {
+        private static readonly string[] morse = new[] { ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.." };
+
+        public int UniqueMorseRepresentations(string[] words)
+        {
+            var set = new HashSet<string>();
+
+            foreach(var word in words)
+            {
+                var wordMorse = string.Empty;
+                foreach(var c in word)
+                {
+                    wordMorse += morse[c - 'a'];
+                }
+
+                set.Add(wordMorse);
+            }
+
+            return set.Count;
+        }
+    }
+}
---
+++ LeetCode/Problem804/SolutionTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentAssertions;
+using Xunit;
+
+namespace LeetCode.Problem804
+{
+    public class SolutionTest
+    {
+        [Theory]
+        [MemberData(nameof(TestCases))]
+        public void Test(string[] words, int expected)
+        {
+            new Solution().UniqueMorseRepresentations(words).Should().Be(expected);
+        }
+
+        public static TheoryData<string[], int> TestCases()
+        {
+            var data = new TheoryData<string[], int>
+            {
+                { new string[0], 0 },
+                { new [] { "gin", "zen", "gig", "msg" }, 2 },
+            };
+            return data;
+        }
+    }
+}
