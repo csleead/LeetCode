d1fa541d0f2416e31dbb4ceaa153656a1006b3b2
Gary Lee <leechisan2007@hotmail.com>
2019-01-30 21:26:23 +0800

    977

---
+++ LeetCode/Problem977/Solution.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Problem977
+{
+    public class Solution
+    {
+        public int[] SortedSquares(int[] A)
+        {
+            var result = new int[A.Length];
+            int i = result.Length - 1;
+
+            int low = 0;
+            int up = A.Length - 1;
+            while(low <= up)
+            {
+                if(A[low] * A[low] > A[up] * A[up])
+                {
+                    result[i] = A[low] * A[low];
+                    low++;
+                }
+                else
+                {
+                    result[i] = A[up] * A[up];
+                    up--;
+                }
+                i--;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ LeetCode/Problem977/SolutionTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentAssertions;
+using Xunit;
+
+namespace LeetCode.Problem977
+{
+    public class SolutionTest
+    {
+        public static TheoryData<int[], int[]> TestCases()
+        {
+            var data = new TheoryData<int[], int[]>
+            {
+                { new int[0], new int[0] },
+                { new[] { 1 }, new[] { 1 } },
+                { new[] { -1 }, new[] { 1 } },
+                { new[] { 0 }, new[] { 0 } },
+                { new[] { -4, -1, 0, 3, 10 }, new[] { 0, 1, 9, 16, 100 } },
+                { new[] { -7, -3, 2, 3, 11 }, new[] { 4, 9, 9, 49, 121 } },
+                { new[] { 1,2,3 }, new[] { 1,4,9 } },
+                { new[] { -3,-2,-1 }, new[] { 1,4,9 } },
+            };
+
+            return data;
+        }
+
+        [Theory]
+        [MemberData(nameof(TestCases))]
+        public void Test(int[] input, int[] expected)
+        {
+            var actual = new Solution().SortedSquares(input);
+
+            actual.ShouldBeEquivalentTo(expected, options => options.WithStrictOrdering());
+        }
+    }
+}
