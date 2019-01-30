ed17f4871daa8ed705a9416a9ecd95e86cfdc500
Gary Lee <leechisan2007@hotmail.com>
2019-01-30 20:55:55 +0800

    709

---
+++ LeetCode/Problem709/Solution.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Problem709
+{
+    public class Solution
+    {
+        public string ToLowerCase(string str)
+        {
+            const int offset = 'a' - 'A';
+            var arr = str.ToCharArray();
+            for(int i = 0; i < arr.Length; i++)
+            {
+                if(arr[i] >= 'A' && arr[i] <= 'Z')
+                {
+                    arr[i] = (char)(arr[i] + offset);
+                }
+            }
+
+            return new string(arr);
+        }
+    }
+}
