9daa2e46448d17d0dacd3591050c628ee28d28c8
Gary Lee <leechisan2007@hotmail.com>
2019-01-31 23:25:33 +0800

    942

---
+++ LeetCode/Problem942/Solution.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Problem942
+{
+    // Given a string S that only contains "I" (increase) or "D" (decrease), let N = S.length.
+    // Return any permutation A of [0, 1, ..., N] such that for all i = 0, ..., N-1:
+    // - If S[i] == "I", then A[i] < A[i+1]
+    // If S[i] == "D", then A[i] > A[i+1]
+    // Inputs:
+    // - 1 <= S.length <= 10000
+    // - S only contains characters "I" or "D"
+    public class Solution
+    {
+        public int[] DiStringMatch(string S)
+        {
+            int dCount = 0;
+            foreach(var c in S)
+            {
+                if(c == 'D')
+                {
+                    dCount++;
+                }
+            }
+
+            var arr = new int[S.Length + 1];
+            arr[0] = dCount;
+            int min = dCount, max = dCount;
+            for(int i = 1; i < arr.Length; i++)
+            {
+                if(S[i - 1] == 'I')
+                {
+                    arr[i] = max + 1;
+                    max++;
+                }
+                else
+                {
+                    arr[i] = min - 1;
+                    min--;
+                }
+            }
+
+            return arr;
+        }
+    }
+}
---
+++ LeetCode/Problem942/SolutionTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+using FluentAssertions;
+using Xunit.Abstractions;
+
+namespace LeetCode.Problem942
+{
+    public class SolutionTest
+    {
+        private readonly ITestOutputHelper _output;
+
+        public SolutionTest(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        [Theory]
+        [InlineData("III")]
+        [InlineData("IID")]
+        [InlineData("IDI")]
+        [InlineData("IDD")]
+        [InlineData("DID")]
+        [InlineData("DDI")]
+        [InlineData("DII")]
+        [InlineData("DDD")]
+        public void Test(string input)
+        {
+            var result = new Solution().DiStringMatch(input);
+
+            result.Sum().Should().Be(Enumerable.Range(0, input.Length + 1).Sum());
+
+            _output.WriteLine(string.Join(", ", result));
+            for(int i = 0; i < input.Length - 1; i++)
+            {
+                if(input[i] == 'I')
+                {
+                    result[i].Should().BeLessThan(result[i + 1]);
+                }
+                else
+                {
+                    result[i].Should().BeGreaterThan(result[i + 1]);
+                }
+            }
+        }
+    }
+}
