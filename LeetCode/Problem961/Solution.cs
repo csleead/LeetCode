66a35a200eb4fc5e56055969cd33d7b0f955b37f
Gary Lee <leechisan2007@hotmail.com>
2019-01-31 21:05:18 +0800

    961

---
+++ LeetCode/Problem961/Solution.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Problem961
+{
+    public class Solution
+    {
+        // In a array A of size 2N, there are N+1 unique elements, and exactly one of these elements is repeated N times.
+        // Return the element repeated N times.
+        // Input assumptions:
+        // - 4 <= A.length <= 10000
+        // - 0 <= A[i] < 10000
+        // - A.length is even
+        public int RepeatedNTimes(int[] A)
+        {
+            // Since half of the array elements are the repeated number.
+            // if A.Length != 4, we can always find two adjacent repeated
+            // elements seperated by at most 2
+            for(int i = 0; i < A.Length - 2; i++)
+            {
+                if(A[i] == A[i + 1] || A[i] == A[i + 2])
+                {
+                    return A[i];
+                }
+            }
+
+            // Special cases:
+            // [x,a,b,x], "x" is seperated by 3
+            // [a,b,x,x] or [x,a,b,c,x,x], the above loop only reach "b" or "c"
+            // The last element is always the repeated one no matther which case it is
+            return A[A.Length - 1];
+        }
+    }
+}
---
+++ LeetCode/Problem961/SolutionTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentAssertions;
+using Xunit;
+
+namespace LeetCode.Problem961
+{
+    public class SolutionTest
+    {
+        public static TheoryData<int[], int> TestCases()
+        {
+            var data = new TheoryData<int[], int>
+            {
+                { new[] { 2, 1, 3, 2 }, 2 },
+                { new[] { 1, 2, 3, 3 }, 3 },
+                { new[] { 2,1,2,5,3,2 }, 2 },
+                { new[] { 5,1,5,2,5,3,5,4 }, 5 },
+            };
+
+            return data;
+        }
+
+        [Theory]
+        [MemberData(nameof(TestCases))]
+        public void Test(int[] input, int expected)
+        {
+            var actual = new Solution().RepeatedNTimes(input);
+
+            actual.Should().Be(expected);
+        }
+    }
+}
