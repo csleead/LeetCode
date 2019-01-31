cfd8d82e8693ac8d31546474675a4045f3b5b7e0
Gary Lee <leechisan2007@hotmail.com>
2019-01-31 22:27:03 +0800

    832

---
+++ LeetCode/Problem832/Solution.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Problem832
+{
+    public class Solution
+    {
+        // Given a binary matrix A, we want to flip the image horizontally, then invert it, and return the resulting image.
+        // To flip an image horizontally means that each row of the image is reversed. For example, flipping [1, 1, 0] horizontally results in [0, 1, 1].
+        // To invert an image means that each 0 is replaced by 1, and each 1 is replaced by 0. For example, inverting [0, 1, 1] results in [1, 0, 0].
+        // input assumptions:
+        // - A is a square matrix
+        // - A.Length >= 1
+        // - A[i][j] must be 0 or 1
+        public int[][] FlipAndInvertImage(int[][] A)
+        {
+            int n = A.Length;
+
+            for(int row = 0; row < n; row++)
+            {
+                for(int col = 0; col < n / 2; col++)
+                {
+                    int left = A[row][col];
+                    int right = A[row][n - col - 1];
+
+                    A[row][col] = Invert(right);
+                    A[row][n - col - 1] = Invert(left);
+                }
+
+                if(n % 2 == 1)
+                {
+                    A[row][n / 2] = Invert(A[row][n / 2]);
+                }
+            }
+
+            return A;
+        }
+
+        private int Invert(int i)
+        {
+            return i == 0 ? 1 : 0;
+        }
+    }
+}
---
+++ LeetCode/Problem832/SolutionTest.cs
@@ -0,0 +1,80 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode.Problem832
+{
+    public class SolutionTest
+    {
+        public static TheoryData<int[][], int[][]> TestCases()
+        {
+            var data = new TheoryData<int[][], int[][]>
+            {
+                // case 1
+                {
+                    new[]{
+                        new[]{1,1,0},
+                        new[]{1,0,1},
+                        new[]{0,0,0}
+                },
+                    new[]{
+                        new[]{1,0,0},
+                        new[]{0,1,0},
+                        new[]{1,1,1}}
+                },
+                // case 2
+                {
+                    new[]{
+                       new[]{1,1,0,0},
+                       new[]{1,0,0,1},
+                       new[]{0,1,1,1},
+                       new[]{1,0,1,0}
+                    },
+                    new[]{
+                       new[]{1,1,0,0},
+                       new[]{0,1,1,0},
+                       new[]{0,0,0,1},
+                       new[]{1,0,1,0}
+                    }
+                },
+                // case 3
+                {
+                    new[]{
+                       new[]{1}
+                    },
+                    new[]{
+                       new[]{0}
+                    }
+                },
+                // case 4
+                 {
+                    new[]{
+                       new[]{1, 0},
+                       new[]{0, 1},
+                    },
+                    new[]{
+                       new[]{1, 0},
+                       new[]{0, 1},
+                    }
+                },
+            };
+
+            return data;
+        }
+
+        [Theory]
+        [MemberData(nameof(TestCases))]
+        public void Test(int[][] input, int[][] expected)
+        {
+            var actual = new Solution().FlipAndInvertImage(input);
+
+            int n = input.Length;
+            for(int i = 0; i < n; i++)
+            {
+                for(int j = 0; j < n; j++)
+                {
+                    actual[i][j].Should().Be(expected[i][j], $"[{i}][{j}] not equals");
+                }
+            }
+        }
+    }
+}
