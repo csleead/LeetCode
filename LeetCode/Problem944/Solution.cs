4bc8d2d99017a609266a3e75b3242ee99893ba24
Gary Lee <leechisan2007@hotmail.com>
2019-02-01 22:36:03 +0800

    944

---
+++ LeetCode/Problem944/Solution.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Problem944
+{
+    public class Solution
+    {
+        // We are given an array A of N lowercase letter strings, all of the same length.
+        // Now, we may choose any set of deletion indices, and for each string, we delete all the characters in those indices.
+        // For example, if we have an array A = ["abcdef","uvwxyz"] and deletion indices {0, 2, 3},
+        // then the final array after deletions is ["bef", "vyz"], and the remaining columns of A are
+        // ["b","v"], ["e","y"], and ["f","z"].  (Formally, the c-th column is [A[0][c], A[1][c], ..., A[A.length-1][c]].)
+        // Suppose we chose a set of deletion indices D such that after deletions, each remaining column in A is in non-decreasing sorted order.
+        // Return the minimum possible value of D.length.
+        // Input:
+        // - 1 <= A.length <= 100
+        // - 1 <= A[i].length <= 1000
+        public int MinDeletionSize(string[] A)
+        {
+            int colCount = A[0].Length;
+
+            int result = 0;
+            for(int col = 0; col < colCount; col++)
+            {
+                for(int row = 0; row < A.Length - 1; row++)
+                {
+                    if(A[row][col] > A[row + 1][col])
+                    {
+                        result++;
+                        break;
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ LeetCode/Problem944/SolutionTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentAssertions;
+using Xunit;
+
+namespace LeetCode.Problem944
+{
+    public class SolutionTest
+    {
+        public static TheoryData<string[], int> TestCases()
+        {
+            var data = new TheoryData<string[], int>
+            {
+                { new[] { "cba","daf","ghi" }, 1 },
+                { new[] { "a","b" }, 0 },
+                { new[] { "zyx","wvu","tsr" }, 3 },
+                { new[] { "ab" }, 0 },
+            };
+
+            return data;
+        }
+
+        [Theory]
+        [MemberData(nameof(TestCases))]
+        public void Test(string[] input, int expected)
+        {
+            var actual = new Solution().MinDeletionSize(input);
+
+            actual.Should().Be(expected);
+        }
+    }
+}
