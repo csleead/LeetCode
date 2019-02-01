0fbb9421c8d0ae9b0c98ab19e5878d0d231f6f8d
Gary Lee <leechisan2007@hotmail.com>
2019-02-01 22:25:20 +0800

    852

---
+++ LeetCode/Problem852/Solution.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Problem852
+{
+    public class Solution
+    {
+        // Given an array that is definitely a mountain, return any i such that
+        // A[0] < A[1] < ... A[i-1] < A[i] > A[i+1] > ... > A[A.length - 1].
+        public int PeakIndexInMountainArray(int[] A)
+        {
+            for(int i = 1; i < A.Length - 1; i++)
+            {
+                if(A[i - 1] < A[i] && A[i] > A[i + 1])
+                    return i;
+            }
+
+            throw new Exception("never");
+        }
+    }
+}
