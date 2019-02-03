39cea3c35302fca905d7ca58e7e015082eecede8
Gary Lee <leechisan2007@hotmail.com>
2019-02-03 16:01:33 +0800

    973

---
+++ LeetCode/Problem973/Solution.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Problem973
+{
+    // We have a list of points on the plane.  Find the K closest points to the origin (0, 0).
+    // (Here, the distance between two points on a plane is the Euclidean distance.)
+    // You may return the answer in any order.  The answer is guaranteed to be unique (except for the order that it is in.)
+    // Inputs
+    // - 1 <= K <= points.length <= 10000
+    // - -10000 < points[i][0] < 10000
+    // - -10000 < points[i][1] < 10000
+    public class Solution
+    {
+        public int[][] KClosest(int[][] points, int K)
+        {
+            if(points.Length <= K)
+            {
+                return points;
+            }
+
+            int from = 0, to = points.Length - 1;
+            int pivotIndex;
+            while((pivotIndex = Partition(points, from, to)) != K - 1)
+            {
+                if(pivotIndex > K - 1)
+                {
+                    to = pivotIndex - 1;
+                }
+                else
+                {
+                    from = pivotIndex + 1;
+                }
+            }
+
+            var result = new int[K][];
+            Array.Copy(points, result, K);
+            return result;
+        }
+
+        public int Partition(int[][] points, int from, int to)
+        {
+            if(from + 1 == to)
+            {
+                if(Distance(points[from]) < Distance(points[to]))
+                {
+                    return from;
+                }
+                else
+                {
+                    Swap(points, from, to);
+                    return to;
+                }
+            }
+
+            int pivotDist = Distance(points[from]);
+            int low = from + 1, high = to;
+
+            while(low < high)
+            {
+                while(low < to && Distance(points[low]) <= pivotDist)
+                {
+                    low++;
+                }
+
+                while(high > from && Distance(points[high]) > pivotDist)
+                {
+                    high--;
+                }
+
+                if(low < high)
+                {
+                    Swap(points, low, high);
+                }
+            }
+
+            Swap(points, from, high);
+
+            return high;
+        }
+
+        private int Distance(int[] point)
+        {
+            return point[0] * point[0] + point[1] * point[1];
+        }
+
+        private void Swap(int[][] points, int i, int j)
+        {
+            var temp = points[i];
+            points[i] = points[j];
+            points[j] = temp;
+        }
+    }
+}
---
+++ LeetCode/Problem973/SolutionTest.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using FluentAssertions;
+using System.Linq;
+
+namespace LeetCode.Problem973
+{
+    public class SolutionTest
+    {
+        public static TheoryData<Point[], int, Point[]> TestCases()
+        {
+            var data = new TheoryData<Point[], int, Point[]>
+            {
+                { new[] { new Point(1,1) }, 1, new[] { new Point(1,1) } },
+                { new[] { new Point(1, 1), new Point(2, 2) }, 1, new[] { new Point(1, 1) } },
+                { new[] { new Point(1, 1), new Point(2, 2) }, 2, new[] { new Point(1, 1), new Point(2, 2) } },
+                {
+                    new[] { new Point(1, 1), new Point(2, 2),  new Point(3, 3), new Point(4, 4),  new Point(5, 5), new Point(6, 6), },
+                    3,
+                    new[] { new Point(1, 1), new Point(2, 2), new Point(3, 3) }
+                },
+                { new[] { new Point(1, 1) }, 999, new[] { new Point(1, 1) } },
+                {
+                    new[] { new Point(5, 5), new Point(4, 4), new Point(3, 3), new Point(2, 2),  new Point(1, 1), },
+                    2,
+                    new[] { new Point(1, 1), new Point(2, 2) }
+                },
+            };
+
+            return data;
+        }
+
+        [Theory]
+        [MemberData(nameof(TestCases))]
+        public void Test(Point[] input, int k, Point[] expected)
+        {
+            var actualArray = new Solution().KClosest(input.Select(arr => arr.ToArray()).ToArray(), k);
+
+            var actualPoints = actualArray.Select(arr => new Point(arr)).ToArray();
+            actualPoints.Should().BeEquivalentTo(expected);
+        }
+
+        public class Point : IEquatable<Point>
+        {
+            public int X { get; }
+            public int Y { get; }
+
+            public Point(int x, int y)
+            {
+                X = x;
+                Y = y;
+            }
+
+            public Point(int[] arr)
+            {
+                X = arr[0];
+                Y = arr[1];
+            }
+
+            public int[] ToArray() => new[] { X, Y };
+
+            public override bool Equals(object obj) => Equals(obj as Point);
+
+            public bool Equals(Point other)
+            {
+                return other != null &&
+                       X == other.X &&
+                       Y == other.Y;
+            }
+
+            public override int GetHashCode()
+            {
+                var hashCode = 1861411795;
+                hashCode = hashCode * -1521134295 + X.GetHashCode();
+                hashCode = hashCode * -1521134295 + Y.GetHashCode();
+                return hashCode;
+            }
+
+            public override string ToString() => $"({X},{Y}), Dist = {X * X + Y * Y}";
+        }
+    }
+}
