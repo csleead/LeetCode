5dedec62e2b895f2127534ff1e9a1d40990b870d
Gary Lee <leechisan2007@hotmail.com>
2019-02-10 13:38:05 +0800

    883

---
+++ LeetCode/Problem883/Solution.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace LeetCode.Problem883
+{
+    public class Solution
+    {
+        public int ProjectionArea(int[][] grid)
+        {
+            return TopViewArea(grid) + SideViewArea(grid) + FrontViewArea(grid);
+        }
+
+        private int TopViewArea(int[][] grid)
+        {
+            return grid.SelectMany(g => g).Count(i => i > 0);
+        }
+
+        private int SideViewArea(int[][] grid)
+        {
+            return grid.Select(row => row.Max()).Sum();
+        }
+
+        private int FrontViewArea(int[][] grid)
+        {
+            int sum = 0;
+
+            int n = grid.Length;
+            for(int col = 0; col < n; col++)
+            {
+                int max = 0;
+                for(int row = 0; row < n; row++)
+                {
+                    max = Math.Max(grid[row][col], max);
+                }
+                sum += max;
+            }
+
+            return sum;
+        }
+    }
+}
---
+++ LeetCode/Problem883/SolutionTest.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode.Problem883
+{
+    public class SolutionTest
+    {
+        public static TheoryData<int[][], int> TestCases()
+        {
+            var data = new TheoryData<int[][], int>
+            {
+                { new[] { new[] { 2 } }, 5 },
+                { new[] { new[] { 1, 2 }, new[] { 3, 4 } }, 17 },
+                { new[] { new[] { 1, 0 }, new[] { 0, 2 } }, 8 },
+                { new[] { new[] { 1,1,1 }, new[] { 1,0,1 }, new[] { 1, 1, 1 } }, 14 },
+                { new[] { new[] { 2,2,2 }, new[] { 2,1,2 }, new[] { 2, 2, 2 } }, 21 },
+            };
+
+            return data;
+        }
+
+        [Theory]
+        [MemberData(nameof(TestCases))]
+        public void Test(int[][] grid, int expected)
+        {
+            var actual = new Solution().ProjectionArea(grid);
+            actual.Should().Be(expected);
+        }
+    }
+}
