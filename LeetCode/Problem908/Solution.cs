e4595661b6bf8dc2a2329cab546b7a83b70bb6ff
Gary Lee <leechisan2007@hotmail.com>
2019-02-10 15:02:56 +0800

    908

---
+++ LeetCode/Problem908/Solution.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Problem908
+{
+    public class Solution
+    {
+        // Given an array A of integers, for each integer A[i] we may choose any x with -K <= x <= K, and add x to A[i].
+        // After this process, we have some array B.
+        // Return the smallest possible difference between the maximum value of B and the minimum value of B.
+        public int SmallestRangeI(int[] A, int K)
+        {
+            if(A.Length == 1)
+                return 0;
+
+            FindMinAndMax(A, out var min, out var max);
+            return Math.Max(0, max - min - 2 * K);
+        }
+
+        private void FindMinAndMax(int[] A, out int min, out int max)
+        {
+            min = int.MaxValue;
+            max = int.MinValue;
+            foreach(var i in A)
+            {
+                min = Math.Min(min, i);
+                max = Math.Max(max, i);
+            }
+        }
+    }
+}
---
+++ LeetCode/Problem908/SolutionTest.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode.Problem908
+{
+    public class SolutionTest
+    {
+        public static TheoryData<int[], int, int> TestCases()
+        {
+            var data = new TheoryData<int[], int, int>
+            {
+                { new[] { 1 }, 1, 0 },
+                { new[] { 0,10 }, 2, 6 },
+                { new[] { 1,3,6 }, 3, 0 },
+                { new[] { 0, 10, 18 }, 4, 10 },
+                { new[] { 0, 10, 18 }, 3, 12 },
+            };
+
+            return data;
+        }
+
+        [Theory]
+        [MemberData(nameof(TestCases))]
+        public void Test(int[] A, int K, int expected)
+        {
+            var actual = new Solution().SmallestRangeI(A, K);
+            actual.Should().Be(expected);
+        }
+    }
+}
