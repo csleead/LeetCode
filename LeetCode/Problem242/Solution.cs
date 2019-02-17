89241cc713e0648e27fccf3cf42536a1174a0122
Gary Lee <leechisan2007@hotmail.com>
2019-02-17 19:08:22 +0800

    242

---
+++ LeetCode/Problem242/Solution.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Problem242
+{
+    public class Solution
+    {
+        // inputs only contain lowercase letter
+        public bool IsAnagram(string s, string t)
+        {
+            if(s.Length != t.Length)
+                return false;
+
+            if(s.Length == 0)
+                return true;
+
+            var sArr = new int[26];
+            var tArr = new int[26];
+            Array.Fill(sArr, 0);
+            Array.Fill(tArr, 0);
+
+            int n = s.Length;
+            for(int i = 0; i < n; i++)
+            {
+                sArr[s[i] - 'a']++;
+                tArr[t[i] - 'a']++;
+            }
+
+            for(int i = 0; i < 26; i++)
+            {
+                if(sArr[i] != tArr[i])
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ LeetCode/Problem242/SolutionTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentAssertions;
+using Xunit;
+
+namespace LeetCode.Problem242
+{
+    public class SolutionTest
+    {
+        [Theory]
+        [InlineData("", "", true)]
+        [InlineData("a", "a", true)]
+        [InlineData("a", "aa", false)]
+        [InlineData("a", "b", false)]
+        [InlineData("anagram", "nagaram", true)]
+        public void Test(string s, string t, bool expected)
+        {
+            new Solution().IsAnagram(s, t).Should().Be(expected);
+        }
+    }
+}
