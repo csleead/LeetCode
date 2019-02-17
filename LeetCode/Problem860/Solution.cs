fa0ab449613f314301c3ef7966e35c079066a792
Gary Lee <leechisan2007@hotmail.com>
2019-02-17 23:36:02 +0800

    860

---
+++ LeetCode/Problem860/Solution.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Problem860
+{
+    public class Solution
+    {
+        // At a lemonade stand, each lemonade costs $5.
+        // Customers are standing in a queue to buy from you, and order one at a time(in the order specified by bills).
+        // Each customer will only buy one lemonade and pay with either a $5, $10, or $20 bill.
+        // You must provide the correct change to each customer, so that the net transaction is that the customer pays $5.
+        // Note that you don't have any change in hand at first.
+        // Return true if and only if you can provide every customer with correct change.
+        public bool LemonadeChange(int[] bills)
+        {
+            int count5 = 0, count10 = 0, count20 = 0;
+
+            foreach(var b in bills)
+            {
+                if(b == 5)
+                {
+                    count5++;
+                }
+                else if(b == 10)
+                {
+                    if(count5 > 0)
+                    {
+                        count10++;
+                        count5--;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    // b == 20
+                    if(count10 > 0 && count5 > 0)
+                    {
+                        count20++;
+                        count5--;
+                        count10--;
+                    }
+                    else if(count5 >= 3)
+                    {
+                        count20++;
+                        count5 -= 3;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ LeetCode/Problem860/SolutionTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentAssertions;
+using Xunit;
+
+namespace LeetCode.Problem860
+{
+    public class SolutionTest
+    {
+        public static TheoryData<int[], bool> TestCases()
+        {
+            var data = new TheoryData<int[], bool>
+            {
+                { new[] { 5,5,5,10,20 }, true },
+                { new[] { 5,5,10 }, true },
+                { new[] { 10,10 }, false },
+                { new[] { 5,5,10,10,20 }, false }
+            };
+
+            return data;
+        }
+
+        [Theory]
+        [MemberData(nameof(TestCases))]
+        public void Test(int[] bills, bool expected)
+        {
+            var actual = new Solution().LemonadeChange(bills);
+            actual.Should().Be(expected);
+        }
+    }
+}
