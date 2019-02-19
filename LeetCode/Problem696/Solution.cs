e71ea73d5dd49f9c9fd7762769ccb6488254b194
Gary Lee <leechisan2007@hotmail.com>
2019-02-19 21:59:25 +0800

    696

---
+++ LeetCode/Problem696/Solution.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Problem696
+{
+    // Give a string s, count the number of non-empty (contiguous) substrings
+    // that have the same number of 0's and 1's, and all the 0's and all the
+    // 1's in these substrings are grouped consecutively.
+    // Substrings that occur multiple times are counted the number of times they occur.
+    public class Solution
+    {
+        public int CountBinarySubstrings(string s)
+        {
+            int result = 0;
+
+            var currentChar = s[0];
+            int charCount1 = 0, charCount2 = 0;
+            foreach(var c in s)
+            {
+                if(c == currentChar)
+                {
+                    charCount2++;
+                }
+                else
+                {
+                    result += Math.Min(charCount1, charCount2);
+                    charCount1 = charCount2;
+                    charCount2 = 1;
+                    currentChar = c;
+                }
+            }
+
+            result += Math.Min(charCount1, charCount2);
+            return result;
+        }
+    }
+}
---
+++ LeetCode/Problem696/SolutionTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentAssertions;
+using Xunit;
+
+namespace LeetCode.Problem696
+{
+    public class SolutionTest
+    {
+        [Theory]
+        [InlineData("0", 0)]
+        [InlineData("01", 1)]
+        [InlineData("011", 1)]
+        [InlineData("101", 2)]
+        [InlineData("1010", 3)]
+        [InlineData("1100", 2)]
+        [InlineData("00110", 3)]
+        public void Test(string input, int expected)
+        {
+            var actual = new Solution().CountBinarySubstrings(input);
+            actual.Should().Be(expected);
+        }
+    }
+}
