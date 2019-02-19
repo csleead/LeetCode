c9b25b7cad31280f3db9b82ef8d7c893ddd4daf3
Gary Lee <leechisan2007@hotmail.com>
2019-02-19 22:56:38 +0800

    733

---
+++ LeetCode/Problem733/Solution.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Problem733
+{
+    public class Solution
+    {
+        public int[,] FloodFill(int[,] image, int sr, int sc, int newColor)
+        {
+            var oldColor = image[sr, sc];
+            var result = image.Clone() as int[,];
+            if(oldColor == newColor)
+            {
+                return result;
+            }
+
+            var stack = new Stack<(int r, int c)>();
+            stack.Push((sr, sc));
+            while(stack.Count > 0)
+            {
+                var (r, c) = stack.Pop();
+                result[r, c] = newColor;
+
+                if(IsInImage(r - 1, c, result) && result[r - 1, c] == oldColor)
+                    stack.Push((r - 1, c));
+
+                if(IsInImage(r + 1, c, result) && result[r + 1, c] == oldColor)
+                    stack.Push((r + 1, c));
+
+                if(IsInImage(r, c - 1, result) && result[r, c - 1] == oldColor)
+                    stack.Push((r, c - 1));
+
+                if(IsInImage(r, c + 1, result) && result[r, c + 1] == oldColor)
+                    stack.Push((r, c + 1));
+            }
+
+            return result;
+        }
+
+        private bool IsInImage(int r, int c, int[,] image)
+        {
+            return r >= 0 && c >= 0 && r < image.GetLength(0) && c < image.GetLength(1);
+        }
+    }
+}
---
+++ LeetCode/Problem733/SolutionTest.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentAssertions;
+using Xunit;
+using System.Linq;
+using Xunit.Sdk;
+
+namespace LeetCode.Problem733
+{
+    public class SolutionTest
+    {
+        public static readonly int NewColor = 65535;
+
+        public static TheoryData<int[,], int, int, int[,]> TestCases()
+        {
+            var data = new TheoryData<int[,], int, int, int[,]>
+            {
+                { new int[,]{ { 1, 0 }, { 0, 1 } }, 0, 0, new int[,]{ { NewColor, 0 }, { 0, 1 } } },
+                { new int[,]{ { 1, 1 }, { 1, 0 } }, 0, 0, new int[,]{ { NewColor, NewColor }, { NewColor, 0 } } },
+                { new int[,]{ { 0, 1, 0 }, { 1, 1, 1 }, { 0, 1, 0 } }, 1, 1, new int[,]{ { 0, NewColor, 0 }, { NewColor, NewColor, NewColor }, { 0, NewColor, 0 } } },
+            };
+
+            return data;
+        }
+
+        [Theory]
+        [MemberData(nameof(TestCases))]
+        public void Test(int[,] input, int sr, int sc, int[,] expected)
+        {
+            var actual = new Solution().FloodFill(input, sr, sc, NewColor);
+
+            AssertEquivalent(actual, expected);
+        }
+
+        [Fact]
+        public void OldColorCanEqaulsToNewColor()
+        {
+            var actual = new Solution().FloodFill(new int[,] { { 0, 0, 0 }, { 0, 1, 1 } }, 1, 1, 1);
+            AssertEquivalent(actual, new int[,] { { 0, 0, 0 }, { 0, 1, 1 } });
+        }
+
+        private void AssertEquivalent(int[,] actual, int[,] expected)
+        {
+            for(int i = 0; i < actual.GetLength(0); i++)
+            {
+                for(int j = 0; j < actual.GetLength(1); j++)
+                {
+                    if(actual[i, j] != expected[i, j])
+                    {
+                        throw new XunitException($"actual = {Format(actual)}, expected = {Format(expected)}");
+                    }
+                }
+            }
+        }
+
+        private string Format(int[,] arr)
+        {
+            var s = new StringBuilder();
+            s.Append("[");
+            for(int i = 0; i < arr.GetLength(0); i++)
+            {
+                s.Append("[");
+                for(int j = 0; j < arr.GetLength(1); j++)
+                {
+                    s.Append(arr[i, j] + ",");
+                }
+                s.Append("],");
+            }
+            s.Append("]");
+
+            return s.ToString();
+        }
+    }
+}
