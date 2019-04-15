73ccfffc4b7e32f211e3ef3e870733afddd025b2
Gary Lee <leechisan2007@hotmail.com>
2019-04-15 22:10:28 +0800

    448

---
+++ LeetCode/Problem448/Solution.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Problem448
+{
+    public class Solution
+    {
+        // same idea as https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/discuss/160034/O(n)-Swap
+        public IList<int> FindDisappearedNumbers(int[] nums)
+        {
+            PutNumbersToCorrectIndex(nums);
+
+            var result = new List<int>();
+            for(int i = 0; i < nums.Length; i++)
+            {
+                if(nums[i] != i + 1)
+                    result.Add(i + 1);
+            }
+
+            return result;
+        }
+
+        private void PutNumbersToCorrectIndex(int[] nums)
+        {
+            int i = 0;
+            while(i < nums.Length)
+            {
+                if(nums[i] == i + 1)
+                {
+                    i++;
+                    continue;
+                }
+
+                var distIndex = nums[i] - 1;
+                if(nums[distIndex] == distIndex + 1)
+                {
+                    i++;
+                    continue;
+                }
+
+                Swap(nums, i, distIndex);
+            }
+        }
+
+        private void Swap(int[] nums, int a, int b)
+        {
+            var tmp = nums[a];
+            nums[a] = nums[b];
+            nums[b] = tmp;
+        }
+    }
+}
---
+++ LeetCode/Problem448/SolutionTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using FluentAssertions;
+
+namespace LeetCode.Problem448
+{
+    public class SolutionTest
+    {
+        public static TheoryData<int[], HashSet<int>> TestData()
+        {
+            var data = new TheoryData<int[], HashSet<int>>
+            {
+                { new[] { 2,2,3 }, new HashSet<int> { 1 } },
+                { new[] { 2,3,2 }, new HashSet<int> { 1 } },
+                { new[] { 3,2,2 }, new HashSet<int> { 1 } },
+                { new[] { 1,2,2 }, new HashSet<int> { 3 } },
+                { new[] { 4, 3, 2, 7, 8, 2, 3, 1 } ,new HashSet<int>{5,6} }
+            };
+            return data;
+        }
+
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public void Test(int[] nums, HashSet<int> expected)
+        {
+            var actual = new HashSet<int>(new Solution().FindDisappearedNumbers(nums));
+            actual.ShouldBeEquivalentTo(expected);
+        }
+    }
+}
