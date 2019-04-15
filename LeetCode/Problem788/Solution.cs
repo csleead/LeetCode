d8d93885e81b5e1627c1fffc45d6e64308a031ef
Gary Lee <leechisan2007@hotmail.com>
2019-04-15 21:30:58 +0800

    788

---
+++ LeetCode/Problem788/Solution.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Problem788
+{
+    public class Solution
+    {
+        public int RotatedDigits(int N)
+        {
+            var result = 0;
+            for(int i = 1; i <= N; i++)
+            {
+                var num = i;
+                bool canRotate = true;
+                bool hasGoodDigit = false;
+                while(num > 0)
+                {
+                    var lastDigit = num % 10;
+
+                    // 3, 4 and 7 cannot be rotated
+                    if(lastDigit == 3 || lastDigit == 4 || lastDigit == 7)
+                    {
+                        canRotate = false;
+                        break;
+                    }
+
+                    if(lastDigit == 2 || lastDigit == 5 || lastDigit == 6 || lastDigit == 9)
+                    {
+                        hasGoodDigit = true;
+                    }
+
+                    num /= 10;
+                }
+
+                if(canRotate && hasGoodDigit)
+                    result++;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ LeetCode/Problem788/SolutionTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using FluentAssertions;
+
+namespace LeetCode.Problem788
+{
+    public class SolutionTest
+    {
+        [Theory]
+        [InlineData(10, 4)]
+        [InlineData(20, 9)]
+        [InlineData(30, 15)]
+        [InlineData(40, 15)]
+        [InlineData(10000, 2320)]
+        public void Test(int N, int expected)
+        {
+            new Solution().RotatedDigits(N).Should().Be(expected);
+        }
+    }
+}
