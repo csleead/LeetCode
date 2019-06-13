1e1c902c5ed45ded6d4183e82664c3ec9af31cf3
Gary Lee <leechisan2007@hotmail.com>
2019-06-13 22:02:49 +0800

    953

---
+++ LeetCode/Problem953/Solution.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Problem953
+{
+    public class Solution
+    {
+        public bool IsAlienSorted(string[] words, string order)
+        {
+            if(words.Length == 1)
+                return true;
+
+            var orderDictionary = CreateOrderDictionary(order);
+
+            for(int i = 1; i < words.Length; i++)
+            {
+                if(!IsSorted(words[i - 1], words[i], orderDictionary))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private static Dictionary<char, int> CreateOrderDictionary(string order)
+        {
+            var orderDictionary = new Dictionary<char, int>(order.Length);
+            for(int i = 0; i < order.Length; i++)
+            {
+                orderDictionary.Add(order[i], i);
+            }
+
+            return orderDictionary;
+        }
+
+        private static bool IsSorted(string s1, string s2, Dictionary<char, int> orderDictionary)
+        {
+            if(s1 == s2)
+                return true;
+
+            for(int i = 0; i < s1.Length && i < s2.Length; i++)
+            {
+                int o1 = orderDictionary[s1[i]], o2 = orderDictionary[s2[i]];
+                if(o1 > o2)
+                    return false;
+                else if(o1 < o2)
+                    return true;
+            }
+
+            return s1.Length < s2.Length;
+        }
+    }
+}
---
+++ LeetCode/Problem953/SolutionTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using FluentAssertions;
+
+namespace LeetCode.Problem953
+{
+    public class SolutionTest
+    {
+        public static TheoryData<string[], string, bool> TestCases()
+        {
+            return new TheoryData<string[], string, bool>
+            {
+                { new[] { "abc" }, "abcdefghijklmnopqrstuvwxyz", true },
+                { new[] { "abc","abc" }, "abcdefghijklmnopqrstuvwxyz", true },
+                { new[] { "abc","abcd" }, "abcdefghijklmnopqrstuvwxyz", true },
+                { new[] { "abcd","abc" }, "abcdefghijklmnopqrstuvwxyz", false },
+                { new[] { "hello", "leetcode" }, "hlabcdefgijkmnopqrstuvwxyz", true },
+                { new[] { "word", "world", "row" }, "worldabcefghijkmnpqstuvxyz", false },
+            };
+        }
+
+        [Theory]
+        [MemberData(nameof(TestCases))]
+        public void Test(string[] words, string order, bool expected)
+        {
+            new Solution().IsAlienSorted(words, order).Should().Be(expected);
+        }
+    }
+}
