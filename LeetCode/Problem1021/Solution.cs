fdb47d0b610024af3b200dc292fd0ab776d04a1b
Gary Lee <leechisan2007@hotmail.com>
2019-06-13 20:53:47 +0800

    1021

---
+++ LeetCode/Problem1021/Solution.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Problem1021
+{
+    public class Solution
+    {
+        public string RemoveOuterParentheses(string S)
+        {
+            var stack = new Stack<char>(S.Length);
+            var list = new List<char>(S.Length);
+            foreach(var c in S)
+            {
+                if(c == '(')
+                {
+                    if(stack.Count != 0)
+                        list.Add(c);
+
+                    stack.Push(c);
+                }
+                else
+                {
+                    if(stack.Count != 1)
+                        list.Add(c);
+
+                    stack.Pop();
+                }
+            }
+
+            return new string(list.ToArray());
+        }
+    }
+}
---
+++ LeetCode/Problem1021/SolutionTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using FluentAssertions;
+
+namespace LeetCode.Problem1021
+{
+    public class SolutionTest
+    {
+        [Theory]
+        [InlineData("(()())(())", "()()()")]
+        [InlineData("(()())(())(()(()))", "()()()()(())")]
+        [InlineData("()()", "")]
+        public void Test(string input, string expected)
+        {
+            new Solution().RemoveOuterParentheses(input).Should().Be(expected);
+        }
+    }
+}
