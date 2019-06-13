ca28c58ff1125ca8b96eb42598339c57e61f4589
Gary Lee <leechisan2007@hotmail.com>
2019-06-13 21:31:18 +0800

    1005

---
+++ LeetCode/Problem1005/Solution.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Problem1005
+{
+    public class Solution
+    {
+        public int LargestSumAfterKNegations(int[] A, int K)
+        {
+            Array.Sort(A);
+
+            var negCount = FindNegativeCount(A);
+
+            if(negCount >= K)
+            {
+                int result = 0;
+                for(int i = 0; i < A.Length; i++)
+                {
+                    result += (i < K) ? -1 * A[i] : A[i];
+                }
+
+                return result;
+            }
+            else
+            {
+                int absSum = A.Sum(i => Math.Abs(i));
+                if((K - negCount) % 2 == 0)
+                {
+                    return absSum;
+                }
+                else
+                {
+                    int minAbs = A.Min(i => Math.Abs(i));
+                    return absSum - 2 * minAbs;
+                }
+            }
+        }
+
+        private int FindNegativeCount(int[] sortedArray)
+        {
+            int count = 0;
+            foreach(var i in sortedArray)
+            {
+                if(i < 0)
+                    count++;
+                else
+                    break;
+            }
+            return count;
+        }
+    }
+}
---
+++ LeetCode/Problem1005/SolutionTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using FluentAssertions;
+
+namespace LeetCode.Problem1005
+{
+    public class SolutionTest
+    {
+        public static TheoryData<int[], int, int> TestCases()
+        {
+            return new TheoryData<int[], int, int>
+            {
+                { new[] { 1 }, 1, -1  },
+                { new[] { 1 }, 2, 1  },
+                { new[] { 1 }, 3, -1  },
+                { new[] { 1,1 }, 1, 0  },
+                { new[] { 2,-3,-1,5,-4 }, 2, 13 }
+            };
+        }
+
+        [Theory]
+        [MemberData(nameof(TestCases))]
+        public void Test(int[] A, int K, int expected)
+        {
+            new Solution().LargestSumAfterKNegations(A, K).Should().Be(expected);
+        }
+    }
+}
