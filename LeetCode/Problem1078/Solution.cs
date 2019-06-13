67f83e3b01ebf5fb1f82119a6acac22abf5aa7a7
Gary Lee <leechisan2007@hotmail.com>
2019-06-13 21:40:00 +0800

    1078

---
+++ LeetCode/Problem1078/Solution.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Problem1078
+{
+    public class Solution
+    {
+        public string[] FindOcurrences(string text, string first, string second)
+        {
+            var words = text.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            if(words.Length < 3)
+                return new string[0];
+
+            var result = new List<string>();
+            for(int i = 2; i < words.Length; i++)
+            {
+                if(words[i - 2] == first && words[i - 1] == second)
+                {
+                    result.Add(words[i]);
+                }
+            }
+
+            return result.ToArray();
+        }
+    }
+}
