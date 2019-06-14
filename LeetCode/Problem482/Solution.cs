0c79158a4a1aac44fee23ec27b67513565d801ab
Gary Lee <leechisan2007@hotmail.com>
2019-06-14 21:55:21 +0800

    482

---
+++ LeetCode/Problem482/Solution.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Problem482
+{
+    public class Solution
+    {
+        public string LicenseKeyFormatting(string S, int K)
+        {
+            var arr = new char[S.Length * 2];
+            var arrIndex = arr.Length - 1;
+
+            int letterCount = 0;
+            for(int i = S.Length - 1; i >= 0; i--)
+            {
+                if(S[i] == '-')
+                    continue;
+
+                if(letterCount > 0 && letterCount % K == 0)
+                {
+                    arr[arrIndex] = '-';
+                    arrIndex--;
+                }
+
+                arr[arrIndex] = ToUpper(S[i]);
+                arrIndex--;
+                letterCount++;
+            }
+
+            return new string(arr, arrIndex + 1, arr.Length - arrIndex - 1);
+        }
+
+        private static char ToUpper(char c)
+        {
+            return (c >= 'a') ? (char)(c - 'a' + 'A') : c;
+        }
+    }
+}
---
+++ LeetCode/Problem482/SolutionTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using FluentAssertions;
+
+namespace LeetCode.Problem482
+{
+    public class SolutionTest
+    {
+        [Theory]
+        [InlineData("5F3Z-2e-9-w", 4, "5F3Z-2E9W")]
+        [InlineData("2-5g-3-J", 2, "2-5G-3J")]
+        [InlineData("5F3Z-2e-9-w", 6, "5F-3Z2E9W")]
+        [InlineData("a-b-c-d", 4, "ABCD")]
+        [InlineData("abcd", 1, "A-B-C-D")]
+        [InlineData("--abcd--", 1, "A-B-C-D")]
+        public void Test(string S, int K, string expected)
+        {
+            new Solution().LicenseKeyFormatting(S, K).Should().Be(expected);
+        }
+    }
+}
