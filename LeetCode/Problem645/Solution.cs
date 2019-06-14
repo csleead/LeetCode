b29e31ec818c82bef2c9eec57a276fe35106d3b0
Gary Lee <leechisan2007@hotmail.com>
2019-06-14 22:31:15 +0800

    645

---
+++ LeetCode/Problem645/Solution.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Problem645
+{
+    public class Solution
+    {
+        public int[] FindErrorNums(int[] nums)
+        {
+            int duplicate = FindDuplicate(nums);
+
+            int sumWithoutError = nums.Length * (nums.Length + 1) / 2;
+            int numsSum = nums.Sum();
+
+            return new[] { duplicate, sumWithoutError + duplicate - numsSum };
+        }
+
+        private int FindDuplicate(int[] nums)
+        {
+            int i = 0;
+            while(true)
+            {
+                if(nums[i] == i + 1)
+                {
+                    i++;
+                }
+                else
+                {
+                    var pos = nums[i] - 1;
+                    if(nums[pos] == nums[i])
+                    {
+                        return nums[i];
+                    }
+                    else
+                    {
+                        int temp = nums[pos];
+                        nums[pos] = nums[i];
+                        nums[i] = temp;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ LeetCode/Problem645/SolutionTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using FluentAssertions;
+
+namespace LeetCode.Problem645
+{
+    public class SolutionTest
+    {
+        [Theory]
+        [InlineData(new[] { 1, 1 }, 1, 2)]
+        [InlineData(new[] { 1, 3, 4, 3 }, 3, 2)]
+        [InlineData(new[] { 3, 1, 4, 3 }, 3, 2)]
+        public void Test(int[] array, int duplicateNumber, int missingNumber)
+        {
+            var result = new Solution().FindErrorNums(array);
+
+            result.Should().HaveCount(2);
+            result[0].Should().Be(duplicateNumber);
+            result[1].Should().Be(missingNumber);
+        }
+    }
+}
