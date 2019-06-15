b3172f0c0b8234a67a565b8d03f0d5525150afe2
Gary Lee <leechisan2007@hotmail.com>
2019-06-15 18:40:00 +0800

    547

---
+++ LeetCode/Problem547/Solution.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Problem547
+{
+    public class Solution
+    {
+        public int FindCircleNum(int[][] M)
+        {
+            var reached = new bool[M.Length];
+            Array.Fill(reached, false);
+
+            int count = 0;
+            for(int i = 0; i < M.Length; i++)
+            {
+                if(!reached[i])
+                {
+                    count++;
+                    DepthFirstSearch(i, M, reached);
+                }
+            }
+
+            return count;
+        }
+
+        private void DepthFirstSearch(int student, int[][] M, bool[] reached)
+        {
+            reached[student] = true;
+
+            for(int i = 0; i < M.Length; i++)
+            {
+                if(!reached[i] && M[student][i] == 1)
+                {
+                    DepthFirstSearch(i, M, reached);
+                }
+            }
+        }
+    }
+}
---
+++ LeetCode/Problem547/SolutionTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using FluentAssertions;
+
+namespace LeetCode.Problem547
+{
+    public class SolutionTest
+    {
+        public static TheoryData<int[][], int> TestCases()
+        {
+            return new TheoryData<int[][], int>
+            {
+                { new[] { new[]{ 1, 0, 0 }, new[]{ 0, 1, 0 }, new[]{ 0, 0, 1 } }, 3 },
+                { new[] { new[]{ 1, 1, 0 }, new[]{ 1, 1, 0 }, new[]{ 0, 0, 1 } }, 2 },
+                { new[] { new[]{ 1, 0, 0, 1 }, new[]{ 0, 1, 1, 0 }, new[]{ 0, 1, 1, 1 }, new[] { 1, 0, 1, 1 } }, 1 },
+            };
+        }
+
+        [Theory]
+        [MemberData(nameof(TestCases))]
+        public void Test(int[][] M, int expected)
+        {
+            new Solution().FindCircleNum(M).Should().Be(expected);
+        }
+    }
+}
