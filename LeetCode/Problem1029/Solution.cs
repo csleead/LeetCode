d70ed65b61d0c5e96d57658fb21a54cd22dd093c
Gary Lee <leechisan2007@hotmail.com>
2019-06-15 19:46:04 +0800

    1029

---
+++ LeetCode/Problem1029/Solution.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace LeetCode.Problem1029
+{
+    public class Solution
+    {
+        public int TwoCitySchedCost(int[][] costs)
+        {
+            return costs
+                .OrderBy(c => c[0] - c[1])
+                .Select((c, i) => (i < costs.Length / 2) ? c[0] : c[1])
+                .Sum();
+        }
+    }
+}
---
+++ LeetCode/Problem1029/SolutionTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentAssertions;
+using Xunit;
+
+namespace LeetCode.Problem1029
+{
+    public class SolutionTest
+    {
+        public static TheoryData<int[][], int> TestCases()
+        {
+            return new TheoryData<int[][], int>
+            {
+                { new[]{ new[]{ 10, 20 }, new[] { 10, 20 } }, 30 },
+                { new[]{ new[]{ 10, 20 }, new[]{ 30, 200 }, new[]{ 400, 50 }, new[]{ 30, 20 } }, 110 },
+                { new[]{ new[]{ 100, 150 }, new[] { 0 , 100 } }, 150 },
+            };
+        }
+
+        [Theory]
+        [MemberData(nameof(TestCases))]
+        public void Test(int[][] costs, int expected)
+        {
+            new Solution().TwoCitySchedCost(costs).Should().Be(expected);
+        }
+    }
+}
