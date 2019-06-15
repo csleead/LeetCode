e0e2608f8b9b07e6c099150857231c9ad9aeef32
Gary Lee <leechisan2007@hotmail.com>
2019-06-15 09:14:53 +0800

    1024

---
+++ LeetCode/Solution1024/Solution.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Solution1024
+{
+    public class Solution
+    {
+        public int VideoStitching(int[][] clips, int T)
+        {
+            var longestStartWith = new Clip[T];
+            Array.Fill(longestStartWith, null);
+
+            foreach(var arr in clips)
+            {
+                var clip = new Clip(arr[0], arr[1]);
+                if(clip.Start >= T)
+                {
+                    continue;
+                }
+
+                if(longestStartWith[clip.Start] == null || longestStartWith[clip.Start].Length < clip.Length)
+                {
+                    longestStartWith[clip.Start] = clip;
+                }
+            }
+
+            int count = 0;
+            int left = -1, right = 0;
+            foreach(var clip in longestStartWith.Where(c => c != null))
+            {
+                if(clip.Start > right)
+                {
+                    return -1;
+                }
+
+                if(clip.End > right)
+                {
+                    if(clip.Start <= left)
+                    {
+                        right = clip.End;
+                    }
+                    else
+                    {
+                        count++;
+                        left = right;
+                        right = clip.End;
+                    }
+                }
+
+                if(right >= T)
+                {
+                    return count;
+                }
+            }
+
+            return -1;
+        }
+
+        private class Clip
+        {
+            public int Start { get; private set; }
+            public int End { get; private set; }
+            public int Length => End - Start;
+
+            public Clip(int start, int end)
+            {
+                Start = start;
+                End = end;
+            }
+        }
+    }
+}
---
+++ LeetCode/Solution1024/SolutionTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace LeetCode.Solution1024
+{
+    public class SolutionTest
+    {
+        public static TheoryData<int[][], int, int> TestCases()
+        {
+            return new TheoryData<int[][], int, int>
+            {
+                { new[] { new[] { 0,1 }, new[] { 1, 2 }  }, 5, -1 },
+                { new[] { new[] { 0,1 }, new[] { 1, 2 }  }, 2, 2 },
+                { new[] { new[] { 0,1 }, new[] { 0, 2 }  }, 2, 1 },
+                { new[] { new[] { 0,1 }, new[] { 1, 2 }, new[] { 2, 3 }  }, 3, 1 },
+                { new[] { new[]{0,2}, new[]{4,6}, new[]{8,10}, new[]{1,9}, new[]{1,5}, new[]{5,9} }, 10, 4 },
+                { new[] { new[]{ 5, 7 }, new[]{ 1, 8 }, new[]{ 0, 0 }, new[]{ 2, 3 }, new[]{ 4, 5 }, new[]{ 0, 6 }, new[]{ 5, 10 }, new[]{ 7, 10 } }, 5, 1 },
+                { new[] { new[]{ 0, 1 }, new[]{ 6, 8 }, new[]{ 0, 2 }, new[]{ 5, 6 }, new[]{ 0, 4 }, new[]{ 0, 3 }, new[]{ 6, 7 }, new[]{ 1, 3 }, new[]{ 4, 7 }, new[]{ 1, 4 }, new[]{ 2, 5 }, new[]{ 2, 6 }, new[]{ 3, 4 }, new[]{ 4, 5 }, new[]{ 5, 7 }, new[]{ 6, 9 } }, 10 , -1 }
+            };
+        }
+    }
+}
