1b376c6a589981226865fd4b1b2c869ebdcb96d8
Gary Lee <leechisan2007@hotmail.com>
2019-06-16 18:47:40 +0800

    742

---
+++ LeetCode/Problem742/Solution.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Problem742
+{
+    public class Solution
+    {
+        public int PivotIndex(int[] nums)
+        {
+            if(nums.Length == 0)
+                return -1;
+
+            int leftSum = nums[0];
+            int rightSum = nums.Sum();
+
+            if(leftSum == rightSum)
+                return 0;
+
+            for(int i = 1; i < nums.Length; i++)
+            {
+                leftSum += nums[i];
+                rightSum -= nums[i - 1];
+
+                if(leftSum == rightSum)
+                    return i;
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ LeetCode/Problem742/SolutionTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentAssertions;
+using Xunit;
+
+namespace LeetCode.Problem742
+{
+    public class SolutionTest
+    {
+        [Theory]
+        [InlineData(new int[] { }, -1)]
+        [InlineData(new[] { 1 }, 0)]
+        [InlineData(new[] { 0, 1, -1 }, 0)]
+        [InlineData(new[] { 1, 7, 3, 6, 5, 6 }, 3)]
+        [InlineData(new[] { -1, -1, -1, 0, 1, 1 }, 0)]
+        public void Test(int[] nums, int expected) =>
+            new Solution().PivotIndex(nums).Should().Be(expected);
+    }
+}
