c4f96f9cc8db0440030d31d6205cbf45eb326b4d
Gary Lee <leechisan2007@hotmail.com>
2019-06-18 21:47:36 +0800

    697

---
+++ LeetCode/Problem697/Solution.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Problem697
+{
+    public class Solution
+    {
+        public int FindShortestSubArray(int[] nums)
+        {
+            var infoDic = new Dictionary<int, Info>();
+
+            int maxDegree = 0;
+            int result = int.MaxValue;
+            for(int i = 0; i < nums.Length; i++)
+            {
+                int n = nums[i];
+
+                var info = infoDic.GetValueOrDefault(n);
+                if(info == null)
+                {
+                    infoDic.Add(n, new Info { FirstOccurenceIndex = i, Degree = 1 });
+                }
+                else
+                {
+                    info.Degree++;
+                    if(info.Degree > maxDegree)
+                    {
+                        maxDegree = info.Degree;
+                        result = i - info.FirstOccurenceIndex + 1;
+                    }
+                    else if(info.Degree == maxDegree)
+                    {
+                        result = Math.Min(result, i - info.FirstOccurenceIndex + 1);
+                    }
+                }
+            }
+
+            return result == int.MaxValue ? 1 : result;
+        }
+
+        private class Info
+        {
+            public int FirstOccurenceIndex { get; set; }
+            public int Degree { get; set; }
+        }
+    }
+}
---
+++ LeetCode/Problem697/SolutionTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using FluentAssertions;
+
+namespace LeetCode.Problem697
+{
+    public class SolutionTest
+    {
+        [Theory]
+        [InlineData(new[] { 1, 2, 2, 3, 1 }, 2)]
+        [InlineData(new[] { 1, 2, 3, 4, 5 }, 1)]
+        [InlineData(new[] { 1, 2, 3, 1, 1 }, 5)]
+        [InlineData(new[] { 1, 2, 2, 2, 1 }, 3)]
+        public void Test(int[] nums, int expected) =>
+            new Solution().FindShortestSubArray(nums).Should().Be(expected);
+    }
+}
