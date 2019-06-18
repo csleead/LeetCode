c132c3369217957ffd6d83ae8a6afa2607bd35f3
Gary Lee <leechisan2007@hotmail.com>
2019-06-18 20:59:12 +0800

    9

---
+++ LeetCode/Problem9/Solution.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Problem9
+{
+    public class Solution
+    {
+        public bool IsPalindrome(int x)
+        {
+            if(x < 0)
+                return false;
+
+            if(x < 10)
+                return true;
+
+            int xx = x;
+            int y = 0;
+            while(xx > 0)
+            {
+                y = 10 * y + xx % 10;
+                xx /= 10;
+            }
+
+            return x == y;
+        }
+    }
+}
---
+++ LeetCode/Problem9/SolutionTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using FluentAssertions;
+using System.Linq;
+
+namespace LeetCode.Problem9
+{
+    public class SolutionTest
+    {
+        [Theory]
+        [InlineData(1)]
+        [InlineData(10)]
+        [InlineData(11)]
+        [InlineData(121)]
+        [InlineData(123454321)]
+        public void Test(int x)
+        {
+            var expected = x.ToString() == new string(x.ToString().ToCharArray().Reverse().ToArray());
+            new Solution().IsPalindrome(x).Should().Be(expected);
+        }
+    }
+}
