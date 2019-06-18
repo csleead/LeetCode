6d25070e2ae0f42e17ed242d6f3ec8f0ff41adbd
Gary Lee <leechisan2007@hotmail.com>
2019-06-18 22:26:43 +0800

    997

---
+++ LeetCode/Problem997/Solution.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Problem997
+{
+    public class Solution
+    {
+        public int FindJudge(int N, int[][] trust)
+        {
+            int[] trustCount = new int[N];
+            Array.Fill(trustCount, 0);
+
+            int[] trustedCount = new int[N];
+            Array.Fill(trustedCount, 0);
+
+            foreach(var t in trust)
+            {
+                trustCount[t[0] - 1]++;
+                trustedCount[t[1] - 1]++;
+            }
+
+            int maybeJudge = -1;
+            for(int i = 0; i < N; i++)
+            {
+                if(trustCount[i] == 0 && trustedCount[i] == N - 1)
+                {
+                    if(maybeJudge == -1)
+                    {
+                        maybeJudge = i + 1;
+                    }
+                    else
+                    {
+                        // two people satisfying criteria of judge
+                        return -1;
+                    }
+                }
+            }
+
+            return maybeJudge;
+        }
+    }
+}
