27082583101a4a51e925ca53d112422418900b61
Gary Lee <leechisan2007@hotmail.com>
2019-06-22 23:25:12 +0800

    876

---
+++ LeetCode/Problem876/Solution.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Problem876
+{
+    public class Solution
+    {
+        public ListNode MiddleNode(ListNode head)
+        {
+            var fast = head;
+            var slow = head;
+
+            while(fast.next != null)
+            {
+                if(fast.next != null)
+                    fast = fast.next;
+
+                if(fast.next != null)
+                    fast = fast.next;
+
+                slow = slow.next;
+            }
+
+            return slow;
+        }
+    }
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+
+        public ListNode(int x)
+        {
+            val = x;
+        }
+    }
+}
---
+++ LeetCode/Problem876/SolutionTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentAssertions;
+using Xunit;
+
+namespace LeetCode.Problem876
+{
+    public class SolutionTest
+    {
+        [Theory]
+        [InlineData(1, 1)]
+        [InlineData(2, 2)]
+        [InlineData(3, 2)]
+        [InlineData(4, 3)]
+        [InlineData(5, 3)]
+        [InlineData(6, 4)]
+        public void Test(int n, int expected)
+        {
+            var list = CreateLinkedList(n);
+            new Solution().MiddleNode(list).val.Should().Be(expected);
+        }
+
+        private ListNode CreateLinkedList(int n)
+        {
+            var root = new ListNode(1);
+
+            var head = root;
+            int i = 2;
+            while(i <= n)
+            {
+                head.next = new ListNode(i);
+                head = head.next;
+
+                i++;
+            }
+
+            return root;
+        }
+    }
+}
