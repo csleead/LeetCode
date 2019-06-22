6db2df030cf31e1f710ff553af1974b2d4ed899e
Gary Lee <leechisan2007@hotmail.com>
2019-06-22 23:49:35 +0800

    1037

---
+++ LeetCode/Problem1037/Solution.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Problem1037
+{
+    public class Solution
+    {
+        public bool IsBoomerang(int[][] points)
+        {
+            return AreDistinctPoints(points) &&
+                Slope(points[0], points[1]) != Slope(points[0], points[2]);
+        }
+
+        private bool AreDistinctPoints(int[][] points)
+        {
+            var p0 = points[0];
+            var p1 = points[1];
+            var p2 = points[2];
+
+            return (p0[0] != p1[0] || p0[1] != p1[1])
+                && (p0[0] != p2[0] || p0[1] != p2[1])
+                && (p1[0] != p2[0] || p1[1] != p2[1]);
+        }
+
+        private double Slope(int[] p1, int[] p2)
+        {
+            if(p1[0] == p2[0])
+                return double.PositiveInfinity;
+            else
+                return ((double)(p1[1] - p2[1])) / (p1[0] - p2[0]);
+        }
+    }
+}
