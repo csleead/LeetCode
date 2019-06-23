ca96c383c036a42253209d78715a0e4b06bb5755
Gary Lee <leechisan2007@hotmail.com>
2019-06-23 13:05:59 +0800

    841

---
+++ LeetCode/Problem841/Solution.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Problem841
+{
+    public class Solution
+    {
+        public bool CanVisitAllRooms(IList<IList<int>> rooms)
+        {
+            var locked = new bool[rooms.Count];
+            Array.Fill(locked, true);
+            locked[0] = false;
+
+            var visited = new bool[rooms.Count];
+            Array.Fill(visited, false);
+
+            var roomsToVisit = new Queue<int>();
+            roomsToVisit.Enqueue(0);
+            while(roomsToVisit.Count > 0)
+            {
+                var room = roomsToVisit.Dequeue();
+                if(visited[room])
+                    continue;
+
+                visited[room] = true;
+
+                if(locked[room])
+                    return false;
+
+                foreach(var key in rooms[room])
+                {
+                    locked[key] = false;
+                    roomsToVisit.Enqueue(key);
+                }
+            }
+
+            return locked.All(b => b == false);
+        }
+    }
+}
---
+++ LeetCode/Problem841/SolutionTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FluentAssertions;
+using Xunit;
+
+namespace LeetCode.Problem841
+{
+    public class SolutionTest
+    {
+        public static TheoryData<int[][], bool> TestCases()
+        {
+            return new TheoryData<int[][], bool>
+            {
+                { new[] { new int[0] } , true },
+                { new[] { new[]{ 1 }, new[]{ 2 }, new[]{ 3 }, new int[0] } , true },
+                { new[] { new[]{ 1 }, new[]{ 2 }, new int[0], new int[0] } , false },
+                { new[] { new[] { 1 }, new[]{ 1 }, new[]{ 2 }, new[] { 3 } } , false },
+                { new[] { new[]{ 1, 3 }, new[]{ 3, 0, 1 }, new[]{ 2 }, new[]{ 0 } } , false },
+            };
+        }
+
+        [Theory]
+        [MemberData(nameof(TestCases))]
+        public void Test(int[][] rooms, bool expected)
+        {
+            var list = new List<IList<int>>();
+            foreach(var r in rooms)
+            {
+                list.Add(r.ToList());
+            }
+
+            new Solution().CanVisitAllRooms(list).Should().Be(expected);
+        }
+    }
+}
