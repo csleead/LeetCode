916789888a6b40d5b603c7200f661cede01c05f2
Gary Lee <leechisan2007@hotmail.com>
2019-06-23 13:18:42 +0800

    931

---
+++ LeetCode/Problem931/Solution.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Problem931
+{
+    public class Solution
+    {
+        public int MinFallingPathSum(int[][] A)
+        {
+            for(int row = 1; row < A.Length; row++)
+            {
+                for(int col = 0; col < A[row].Length; col++)
+                {
+                    int minValueAbove = int.MaxValue;
+
+                    if(col != 0)
+                        minValueAbove = Math.Min(minValueAbove, A[row - 1][col - 1]);
+                    if(col != A[row].Length - 1)
+                        minValueAbove = Math.Min(minValueAbove, A[row - 1][col + 1]);
+                    minValueAbove = Math.Min(minValueAbove, A[row - 1][col]);
+
+                    A[row][col] += minValueAbove;
+                }
+            }
+
+            return A.Last().Min();
+        }
+    }
+}
---
+++ LeetCode/Problem931/SolutionTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentAssertions;
+using Xunit;
+
+namespace LeetCode.Problem931
+{
+    public class SolutionTest
+    {
+        public static TheoryData<int[][], int> TestCases()
+        {
+            return new TheoryData<int[][], int>
+            {
+                { new[]{ new[]{ 1, 2, 3 } } , 1 },
+                { new[]{ new[]{ 1, 2, 3 }, new[]{ 4, 5, 6 }, new[]{ 7, 8, 9 } } , 12 },
+                { new[]{ new[]{ 1, 2, 3 }, new[]{ 5, 4, 6 }, new[]{ 8, 9, 7 } } , 12 },
+            };
+        }
+
+        [Theory]
+        [MemberData(nameof(TestCases))]
+        public void Test(int[][] A, int expected)
+        {
+            new Solution().MinFallingPathSum(A).Should().Be(expected);
+        }
+    }
+}
