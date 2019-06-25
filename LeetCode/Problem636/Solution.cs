a0f7d5bd32a8da53ba5788f554c6ff5503297735
Gary Lee <leechisan2007@hotmail.com>
2019-06-25 22:33:04 +0800

    636

---
+++ LeetCode/Problem636/Solution.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Problem636
+{
+    public class Solution
+    {
+        public int[] ExclusiveTime(int n, IList<string> logs)
+        {
+            var result = new int[n];
+
+            var stack = new Stack<Log>(logs.Count / 2);
+
+            Log previousLog = null;
+            foreach(var log in logs.Select(l => new Log(l)))
+            {
+                if(stack.Count == 0)
+                {
+                    stack.Push(log);
+                }
+                else if(log.Event == Event.Start && previousLog.Event == Event.Start)
+                {
+                    var currentFunction = stack.Peek();
+                    result[currentFunction.FunctionId] += log.Timestamp - previousLog.Timestamp;
+
+                    stack.Push(log);
+                }
+                else if(log.Event == Event.End && previousLog.Event == Event.Start)
+                {
+                    var currentFunction = stack.Pop();
+                    result[currentFunction.FunctionId] += log.Timestamp - previousLog.Timestamp + 1;
+                }
+                else if(log.Event == Event.End && previousLog.Event == Event.End)
+                {
+                    var currentFunction = stack.Pop();
+                    result[currentFunction.FunctionId] += log.Timestamp - previousLog.Timestamp;
+                }
+                else if(log.Event == Event.Start && previousLog.Event == Event.End)
+                {
+                    var currentFunction = stack.Peek();
+                    result[currentFunction.FunctionId] += log.Timestamp - previousLog.Timestamp - 1;
+
+                    stack.Push(log);
+                }
+
+                previousLog = log;
+            }
+
+            return result;
+        }
+
+        private class Log
+        {
+            public int FunctionId { get; set; }
+            public Event Event { get; set; }
+            public int Timestamp { get; set; }
+
+            public Log(string logString)
+            {
+                var firstColon = logString.IndexOf(':');
+                var secondColon = logString.LastIndexOf(':');
+
+                FunctionId = int.Parse(logString.Substring(0, firstColon));
+                Event = logString.Substring(firstColon + 1, secondColon - firstColon - 1) == "start" ? Event.Start : Event.End;
+                Timestamp = int.Parse(logString.Substring(secondColon + 1));
+            }
+        }
+
+        private enum Event
+        {
+            Start,
+            End
+        }
+    }
+}
---
+++ LeetCode/Problem636/SolutionTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentAssertions;
+using Xunit;
+
+namespace LeetCode.Problem636
+{
+    public class SolutionTest
+    {
+        [Theory]
+        [InlineData(1, new[] { "0:start:0", "0:end:0" }, new[] { 1 })]
+        [InlineData(2, new[] { "0:start:0", "0:end:0", "1:start:1", "1:end:1" }, new[] { 1, 1 })]
+        [InlineData(2, new[] { "0:start:0", "1:start:1", "1:end:1", "0:end:2" }, new[] { 2, 1 })]
+        [InlineData(3, new[] { "0:start:0", "1:start:1", "2:start:2", "2:end:3", "1:end:4", "0:end:5" }, new[] { 2, 2, 2 })]
+        [InlineData(1, new[] { "0:start:0", "0:start:2", "0:end:5", "0:start:6", "0:end:6", "0:end:7" }, new[] { 8 })]
+        [InlineData(2, new[] { "0:start:0", "0:start:2", "0:end:5", "1:start:6", "1:end:6", "0:end:7" }, new[] { 7, 1 })]
+        public void Test(int n, IList<string> logs, int[] expected)
+        {
+            new Solution().ExclusiveTime(n, logs)
+                .ShouldBeEquivalentTo(expected, options => options.WithStrictOrdering());
+        }
+    }
+}
