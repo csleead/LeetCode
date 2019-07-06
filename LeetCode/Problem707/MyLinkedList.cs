3ed7167b8ff39863605093c555e97c7ab013e604
Gary Lee <leechisan2007@hotmail.com>
2019-07-06 18:43:27 +0800

    707

---
+++ LeetCode/Problem707/MyLinkedList.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Problem707
+{
+    public class MyLinkedList
+    {
+        private int _count;
+        private Node _dummyHead;
+        private Node _dummyTail;
+
+        public MyLinkedList()
+        {
+            _dummyHead = new Node();
+            _dummyTail = new Node();
+
+            _dummyHead.Next = _dummyTail;
+            _dummyTail.Previous = _dummyHead;
+        }
+
+        public int Get(int index)
+        {
+            var node = FindNodeByIndex(index);
+            return node?.Value ?? -1;
+        }
+
+        public void AddAtHead(int val) => AddAtIndex(0, val);
+
+        public void AddAtTail(int val) => AddAtIndex(_count, val);
+
+        public void AddAtIndex(int index, int val)
+        {
+            if(index > _count)
+                return;
+
+            // For stupid non-sense test case:
+            // ["MyLinkedList","addAtIndex","get"]
+            // [[],[-1,0],[0]]
+            // The Get(0) expected to return 0 instead of -1 header
+            if(index < 0)
+            {
+                AddAtHead(val);
+                return;
+            }
+
+            var next = index == _count ? _dummyTail : FindNodeByIndex(index);
+            var prev = next.Previous;
+
+            var newNode = new Node { Value = val };
+            newNode.Previous = prev;
+            newNode.Next = next;
+
+            prev.Next = next.Previous = newNode;
+
+            _count++;
+        }
+
+        public void DeleteAtIndex(int index)
+        {
+            var node = FindNodeByIndex(index);
+            if(node == null)
+                return;
+
+            var prev = node.Previous;
+            var next = node.Next;
+
+            prev.Next = next;
+            next.Previous = prev;
+
+            _count--;
+        }
+
+        private Node FindNodeByIndex(int index)
+        {
+            if(index < 0 || index >= _count)
+                return null;
+
+            if(index <= _count / 2)
+            {
+                var node = _dummyHead.Next;
+                for(int i = 0; i < index; i++)
+                {
+                    node = node.Next;
+                }
+
+                return node;
+            }
+            else
+            {
+                var node = _dummyTail.Previous;
+                for(int i = _count - 1; i > index; i--)
+                {
+                    node = node.Previous;
+                }
+
+                return node;
+            }
+        }
+
+        private class Node
+        {
+            public Node Previous { get; set; }
+            public Node Next { get; set; }
+            public int Value { get; set; }
+        }
+    }
+}
---
+++ LeetCode/Problem707/MyLinkedListTest.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentAssertions;
+using Xunit;
+
+namespace LeetCode.Problem707
+{
+    public class MyLinkedListTest
+    {
+        [Fact]
+        public void GetReturnsMinusOneForInvalidIndex()
+        {
+            var list = new MyLinkedList();
+            list.AddAtTail(100);
+            list.AddAtTail(200);
+
+            list.Get(-1).Should().Be(-1);
+            list.Get(2).Should().Be(-1);
+        }
+
+        [Fact]
+        public void AddAtTailWorksProperly()
+        {
+            var list = new MyLinkedList();
+
+            list.AddAtTail(100);
+            list.Get(0).Should().Be(100);
+
+            list.AddAtTail(200);
+            list.Get(1).Should().Be(200);
+        }
+
+        [Fact]
+        public void AddAtHeadWorksProperly()
+        {
+            var list = new MyLinkedList();
+
+            list.AddAtHead(100);
+            list.Get(0).Should().Be(100);
+
+            list.AddAtHead(200);
+            list.Get(0).Should().Be(200);
+        }
+
+        [Fact]
+        public void AddAtIndexWorksProperlyIfIndexIsValid()
+        {
+            var list = new MyLinkedList();
+
+            list.AddAtIndex(0, 200);
+            list.Get(0).Should().Be(200);
+
+            list.AddAtIndex(0, 100);
+            list.Get(0).Should().Be(100);
+            list.Get(1).Should().Be(200);
+
+            list.AddAtIndex(1, 150);
+            list.Get(0).Should().Be(100);
+            list.Get(1).Should().Be(150);
+            list.Get(2).Should().Be(200);
+        }
+
+        [Fact]
+        public void AddAtIndexDoNothingIfIndexIsGreaterThanLength()
+        {
+            var list = new MyLinkedList();
+
+            list.AddAtIndex(1, 200);
+            list.Get(0).Should().Be(-1);
+        }
+
+        [Fact]
+        public void AddAtIndexAddToHeadIfIndexIsNegative()
+        {
+            var list = new MyLinkedList();
+
+            list.AddAtIndex(-1, 100);
+            list.Get(0).Should().Be(100);
+        }
+
+        [Fact]
+        public void DeleteAtIndexWorksProperlyIfIndexIsValid()
+        {
+            var list = new MyLinkedList();
+            list.AddAtTail(100);
+            list.AddAtTail(200);
+            list.AddAtTail(300);
+            list.AddAtTail(400);
+
+            list.DeleteAtIndex(0);
+            list.Get(0).Should().Be(200); // [200,300,400]
+
+            list.DeleteAtIndex(1);
+            list.Get(1).Should().Be(400); // [200,400]
+
+            list.DeleteAtIndex(1);
+            list.Get(1).Should().Be(-1); // [200]
+
+            list.DeleteAtIndex(0);
+            list.Get(0).Should().Be(-1); // []
+        }
+
+        [Fact]
+        public void DeleteAtIndexDoNothingIfIndexIsInvalid()
+        {
+            var list = new MyLinkedList();
+            list.AddAtTail(100);
+
+            list.DeleteAtIndex(-1);
+            list.DeleteAtIndex(1);
+
+            list.Get(0).Should().Be(100);
+        }
+    }
+}
