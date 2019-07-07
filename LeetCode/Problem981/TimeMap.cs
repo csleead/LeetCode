ea445e27c31ed47edfb6f7ce3aaea92ece489cf4
Gary Lee <leechisan2007@hotmail.com>
2019-07-07 12:50:15 +0800

    981

---
+++ LeetCode/Problem981/TimeMap.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Problem981
+{
+    public class TimeMap
+    {
+        private readonly Dictionary<string, List<Entry>> _dic = new Dictionary<string, List<Entry>>();
+
+        public void Set(string key, string value, int timestamp)
+        {
+            if(!_dic.ContainsKey(key))
+            {
+                _dic[key] = new List<Entry>();
+            }
+
+            _dic[key].Add(new Entry { Value = value, Timestamp = timestamp });
+        }
+
+        public string Get(string key, int timestamp)
+        {
+            if(_dic.TryGetValue(key, out var list))
+            {
+                var index = list.BinarySearch(new Entry { Timestamp = timestamp });
+                if(index >= 0)
+                {
+                    return list[index].Value;
+                }
+                else
+                {
+                    var nextLargerIndex = ~index;
+                    return nextLargerIndex == 0 ? string.Empty : list[nextLargerIndex - 1].Value;
+                }
+            }
+
+            return string.Empty;
+        }
+
+        private class Entry : IComparable<Entry>
+        {
+            public string Value { get; set; }
+            public int Timestamp { get; set; }
+
+            public int CompareTo(Entry other) => Timestamp - other.Timestamp;
+
+            public override string ToString() => $"{Timestamp}:{Value}";
+        }
+    }
+}
---
+++ LeetCode/Problem981/TimeMapTest.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentAssertions;
+using Xunit;
+
+namespace LeetCode.Problem981
+{
+    public class TimeMapTest
+    {
+        [Fact]
+        public void GetReturnEmptyStringIfNoValueForKey()
+        {
+            var map = new TimeMap();
+            map.Set("key1", "value1", 0);
+
+            map.Get("key2", 0).Should().BeEmpty();
+        }
+
+        [Fact]
+        public void GetReturnEmptyStringIfNoValueSetBeforeThatTimestamp()
+        {
+            var map = new TimeMap();
+            map.Set("key", "value", 100);
+
+            map.Get("key", 99).Should().BeEmpty();
+        }
+
+        [Fact]
+        public void GetReturnThatValueIfValueSetBeforeThatTimestamp()
+        {
+            var map = new TimeMap();
+            map.Set("key", "value", 100);
+
+            map.Get("key", 100).Should().Be("value");
+            map.Get("key", 101).Should().Be("value");
+        }
+
+        [Fact]
+        public void GetReturnThatTheValueWithLatestTimestamp()
+        {
+            var map = new TimeMap();
+            map.Set("key", "value1", 1);
+            map.Set("key", "value2", 2);
+            map.Set("key", "value3", 3);
+
+            map.Get("key", 1).Should().Be("value1");
+            map.Get("key", 2).Should().Be("value2");
+            map.Get("key", 3).Should().Be("value3");
+        }
+    }
+}
